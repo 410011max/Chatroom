4c6d3802b9bf05bbcc7f11f98187f783b293eeb1
TheRookie2021 <96114720+TheRookie2021@users.noreply.github.com>
2022-06-17 17:40:18 +0800

    solved: eliminate the redundant string "You: "

--- server.cpp
+++ server.cpp
@@ -24,14 +24,17 @@ int counter = 0;
 mutex cout_mtx, clients_mtx;
 thread s_ctrl;
 
+
+
 void set_name(int id, char name[]);
 void shared_print(string str, bool endLine);
+// int broadcast_message(string message, int sender_id);
 int broadcast_message(int num, int sender_id);
 void end_connection(int id);
-int find_user_id(string name) void handle_client(int client_socket, int id);
 void handle_client(int client_socket, int id);
 // server control
 void server_control(int server_socket);
+
 
 int main()
 {
@@ -65,9 +68,9 @@ int main()
     socklen_t clnt_addr_size = sizeof(clnt_addr);
     int client_socket;
 
-    // create server control thread
-    thread server_t(server_control, server_socket);
-    s_ctrl = move(server_t);
+    // create server control thread 
+    thread server_t(server_control,server_socket);
+    s_ctrl=move(server_t);
 
     while (1)
     {
@@ -92,9 +95,9 @@ int main()
     }
 
     // join server control thread
-    if (s_ctrl.joinable())
+    if(s_ctrl.joinable())
         s_ctrl.join();
-
+    
     close(client_socket);
     close(server_socket);
 
@@ -134,6 +137,19 @@ int broadcast_message(string message, int sender_id)
         }
     }
 }
+/*
+// Broadcast a number to other clients
+int broadcast_message(int num, int sender_id)
+{
+    for(int i=0; i<clients.size(); i++)
+    {
+        if(clients[i].id!=sender_id)  // 不發送給自己
+        {
+            send(clients[i].socket,&num,sizeof(num),0);
+        }
+    }
+}
+*/
 
 // Clint end connection
 void end_connection(int id)
@@ -151,17 +167,6 @@ void end_connection(int id)
     }
 }
 
-int find_user_id(string name)
-{
-    for (int i = 0; i < clients.size(); i++)
-    {
-        if (name == clients[i].name)
-        {
-            return clientsi[i].id;
-        }
-    }
-}
-
 void handle_client(int client_socket, int id)
 {
     char name[200], str[200];
@@ -170,77 +175,61 @@ void handle_client(int client_socket, int id)
 
     // Display welcome message
     string welcome_message = string("Welcome ") + string(name) + string(" to join OS_2022 Chatroom~");
-    string name_list = "Online users:";
-    for (int i = 0; i < clients.size(); i++)
-    {
-        name_list += " " + clients[i].name;
-    }
-
-    broadcast_message("#NULL", id);         // server 發送之公告，沒有發送者 (client) 名字
-    broadcast_message(welcome_message, id); // 輸出 welcome 訊息到 client
-    broadcast_message("#NULL", id);
-    broadcast_message(name_list, id); // 輸出線上用戶名單到 client
-    shared_print(welcome_message);    // 輸出 welcome 訊息到 server 畫面
-    shared_print(name_list);          // 輸出線上用戶名單到 server 畫面
+    broadcast_message("#NULL",id);
+    // broadcast_message(id,id);
+    broadcast_message(welcome_message, id);
+    // shared_print(color(id)+welcome_message+def_col);
+    shared_print(welcome_message);  // 輸出 welcome 訊息到 server 畫面
 
     while (1)
     {
         int bytes_received = recv(client_socket, str, sizeof(str), 0);
-        if (bytes_received <= 0) // error(-1)或斷開連結(0)
+        if (bytes_received <= 0)
             return;
         if (strcmp(str, "#exit") == 0)
         {
             // Display leaving message
-            string message = string(name) + string(" has left");
-            string name_list = "Online users:";
-            for (int i = 0; i < clients.size(); i++)
-            {
-                name_list += " " + clients[i].name;
-            }
-            broadcast_message("#NULL", id);
-            broadcast_message(message, id); // 輸出離開訊息到 client
-            broadcast_message("#NULL", id);
-            broadcast_message(name_list, id); // 輸出線上用戶名單到 client
+            string message = string(name) + string(" has left");        
+            broadcast_message(message, id);
             shared_print(message);
-            shared_print(name_list);
             end_connection(id);
             return;
         }
         broadcast_message(string(name), id);
+        // broadcast_message(id,id);
         broadcast_message(string(str), id);
+        // shared_print(color(id)+name+" : "+def_col+str);
         shared_print(string(name) + ": " + str);
     }
 }
 
-void server_control(int server_socket)
-{
+void server_control(int server_socket){
     char str[200];
-    while (1)
-    {
+    while(1){
         cin.getline(str, 200);
-        if (strcmp(str, "exit") == 0)
+        if (strcmp(str, "#exit") == 0)
         {
             cout << "close server and clients\n";
 
-            // close clients
+            //close clients
             string message = string("\n\t//////server closed//////\n");
             broadcast_message("#NULL", -1);
-            broadcast_message(message, -1);
+            broadcast_message(message,-1);
             shared_print(message);
 
-            while (clients.size() > 0)
+            while (clients.size()>0)
             {
                 lock_guard<mutex> guard(clients_mtx); // lock 直到清除 client 資料結束
                 clients[0].th.detach();               // 關閉對應thread
                 if (clients[0].th.joinable())
                     clients[0].th.join();
-                close(clients[0].socket);       // Close the client socket
-                clients.erase(clients.begin()); // Erase client information
+                close(clients[0].socket);             // Close the client socket
+                clients.erase(clients.begin());   // Erase client information
             }
 
             // close server
             s_ctrl.detach();
-            if (s_ctrl.joinable())
+            if(s_ctrl.joinable())
                 s_ctrl.join();
             close(server_socket);
 
